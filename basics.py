e11bad4381747f5128679ed4a759f4d79dc83928
88aleksandra88 <aleksandra.slowik88@gmail.com>
2020-10-08 11:37:44 +0200

    switch

---
+++ basics.py
@@ -0,0 +1,39 @@
+glass_1="water"
+glass_2="soda"
+
+temp=glass_1
+glass_1=glass_2
+glass_2=temp
+
+print('glass_1='+str(glass_1))
+//glass_1=soda
+print('glass_2='+str(glass_2))
+//glass_2=water
+
+
+
+
+number_of_steps=70
+print("You're on step:")
+print("number_of_steps +1")
+
+
+
+
+//kilometer convert
+miles=500
+kilometers=miles * 1.609344
+print(kilometers)
+//804.674
+
+
+
+//switch light
+is_day = False
+lights_on = not is_day
+
+print('Daytime?')
+print(is_day)
+
+print('Lights on ?')
+print(lights_on)
