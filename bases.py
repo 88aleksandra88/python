88fa06baa7466e24f114e0fe5312acbf5df3ce49
88aleksandra88 <aleksandra.slowik88@gmail.com>
2020-10-08 09:46:20 +0200

    first commit

---
+++ bases.py
@@ -0,0 +1,9 @@
+glass_1="water"
+glass_2="soda"
+
+temp=glass_1
+glass_1=glass_2
+glass_2=temp
+
+print('glass_1='+str(glass_1))
+print('glass_2='+str(glass_2))
---
+++ readme.pd
@@ -0,0 +1,3 @@
+# PYTHON 
+
+<img src="">
