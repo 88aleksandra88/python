68fc99e93e510a48405dcbfd5119bdff77df6520
88aleksandra88 <aleksandra.slowik88@gmail.com>
2020-10-09 15:11:26 +0200

    end

---  conditional_statement.py
+++  conditional_statement.py
@@ -27,11 +27,43 @@ print(f"{tries - current_try} tries left")
 // 2 tries left 
 
 
+min_age = 18
+max_age = 28
+print(f"{88}% of social media users are aged between {min_age} and {max_age}")
+//88% of social media users are aged between 18 and 28
+
+
 planet = "Jupiter"
 moons = 79
 rings = 4
 print(f"{planet} has {moons} moons and {rings} rings")
 //Jpiter has 79 moons and 4 rings
+
+
+///////////////////Making decisions///////////////////
+greet = True
+if greet :
+    print("Hello world!")
+//Hello world!
+
+greet = False
+if greet :
+    print("Hello world!")
+//none
+
+
+email = "test@test.com"
+is_duplicate = True
+
+if True:
+    print(f"{email} is alredy in use")
+
+
+
+
+
+
+
 
 
 
